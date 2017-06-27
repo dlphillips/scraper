0aee389821637d760d2eb64e7bfc5d87aa052411
Dan Phillips <dlphillips@gmail.com>
2017-06-27 11:51:48 -0400

    site is working as assigned

---
+++ models/Article.js
@@ -0,0 +1,38 @@
+  // Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  // title is a required string
+  title: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  // link is a required string
+  link: {
+    type: String,
+    required: true
+  },
+    imgLink: {
+    type: String,
+    required: true
+  },
+  teaser: {
+    type: String,
+    required: true
+  },
+  // This only saves one note's ObjectId, ref refers to the Note model
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
---
+++ public/js/app.js
@@ -0,0 +1,58 @@
+// Whenever someone clicks a p tag
+
+$(".noteBtn").click(function() {
+    // Empty the notes from the note section
+    $("#notes").empty();
+    // Save the id from the p tag
+    var thisId = $(this).attr("data-id");
+
+    // Now make an ajax call for the Article
+    $.ajax({
+            method: "GET",
+            url: "/articles/" + thisId
+        })
+        // build note modal
+        .done(function(data) {
+            console.log(data);
+            $("#notes").append("<div class='modal-header'><h4 class='modal-title'>" + data.title + "</h4></div>");
+            $("#notes").append("<div class='modal-body'><input id='titleinput' name='title' placeholder='Comment title'><textarea id='bodyinput' name='body' placeholder='Enter comment here'></textarea></div>");
+            $("#notes").append("<div class='modal-footer'><button class='btn btn-primary' data-id='" + data._id + "' id='savenote'>Save Comment</button></div>");
+            if (data.note) {
+                // Place the title of the note in the title input
+                $("#titleinput").val(data.note.title);
+                // Place the body of the note in the body textarea
+                $("#bodyinput").val(data.note.body);
+            }
+            $("#myModal").modal();
+        });
+});
+
+// When you click the savenote button
+$(document).on("click", "#savenote", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+            method: "POST",
+            url: "/articles/" + thisId,
+            data: {
+                // Value taken from title input
+                title: $("#titleinput").val(),
+                // Value taken from note textarea
+                body: $("#bodyinput").val()
+            }
+        })
+        // With that done
+        .done(function(data) {
+            // Log the response
+            console.log(data);
+            // Empty the notes section
+            $("#notes").empty();
+        });
+
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#titleinput").val("");
+    $("#bodyinput").val("");
+    $("#myModal").modal('hide');
+});
